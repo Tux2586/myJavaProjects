d98cc0b138ce9449edfdc7e99420afa965e2d80d
Janardan Kelkar <Tux2586@users.noreply.github.com>
2016-07-06 23:00:13 -0400

    DOne

--- M101J/src/main/java/finalexam/FinalExamSolutions.java
+++ M101J/src/main/java/finalexam/FinalExamSolutions.java
@@ -4,7 +4,10 @@ import static spark.Spark.setPort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -17,58 +20,97 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
-import course.SessionDAO;
- 
 
 public class FinalExamSolutions {
 	
 	protected static MongoCollection<Document> collection = null;
 	
-	private static void Connect(String mongoURI, String dbName){
+	private static void Connect(String mongoURI, String dbName, String collName){
 		final MongoClient mongoClient = new MongoClient(new MongoClientURI(mongoURI));
         final MongoDatabase database = mongoClient.getDatabase(dbName);
-        collection = database.getCollection("messages");
         
-	}
+        if(collection != null){
+        	collection = null;
+        }
+        collection = database.getCollection(collName);
+ 	}
 	
 	
 	public static void main(String[] args) {
-		
-		Connect("mongodb://localhost","enron");
+	/*ENRON	
+		Connect("mongodb://localhost","enron","messages");
 		String[] fromList = {"susan.mara@enron.com","susan.mara@enron.com","soblander@carrfut.com", "susan.mara@enron.com", "evelyn.metoyer@enron.com", "susan.mara@enron.com"};
 		String[] toList = {"jeff.dasovich@enron.com","richard.shapiro@enron.com","soblander@carrfut.com","james.steffes@enron.com","kate.symes@enron.com","alan.comnes@enron.com"};
 		
-		for(short i=0;i<1/*fromList.length*/;i++){
-			System.out.println(fromList[i]+ " <--> " + toList[i] + " : " + getPairCount(fromList[i], toList[i]));
-		}
-		;
+		for(short i=0;i<fromList.length;i++){
+			System.out.println(fromList[i]+ " --> " + toList[i] + " : " + getPairCount(fromList[i], toList[i]));
+		};
+		
+		updateEmailAddress("<8147308.1075851042335.JavaMail.evans@thyme>"); */
+		
+		removeUnTaggedImages();
+		
 		
 	}
 	
-	private static int getPairCount(String email1, String email2){
-		Bson senderFilter = new Document("headers.from",email1);
-		Bson toFilter = Filters.in("headers.to", new ArrayList().add(email2));
-		MongoCursor<Document> cursor = collection.find(Filters.and(senderFilter,toFilter)).iterator();
+	private static int removeUnTaggedImages(){
+		Connect("mongodb://localhost","photoAlbums","albums");
+		MongoCursor<Document> cursor = collection.find().iterator();
+		Set<Integer> imageIDs = new TreeSet<Integer>();
+		while(cursor.hasNext()){
+			Document document = cursor.next();
+			ArrayList<Integer> images = (ArrayList<Integer>)document.get("images");
+			for(Integer imageID:images){
+				imageIDs.add(imageID);
+			}
+		}
+		
+		Connect("mongodb://localhost","photoAlbums","images");
+		Bson filter1 = new Document("tags", new Document("$in",Arrays.asList("kittens")));
+		Bson filter2 = new Document("_id", new Document("$in",new ArrayList(imageIDs)));
+		cursor = collection.find(Filters.and(filter1, filter2)).iterator();
 		int count = 0;
 		
 		while(cursor.hasNext()){
-			cursor.next();
 			count++;
+			cursor.next();
 		}
-		
+		System.out.println(count);
+		return -1;
+	}
+	
+	private static void updateEmailAddress(String messageID){
+		Bson filterDocument = new Document("headers.Message-ID",messageID);
+		Bson updateDocument = new Document("$push",new Document("headers.To","mrpotatohead@mongodb.com"));
+		collection.findOneAndUpdate(filterDocument, updateDocument);
+	}
+	
+	private static int getPairCount(String email1, String email2){
+		Bson senderFilter = new Document("headers.From",email1);
+		Bson toFilter = Filters.in("headers.To", Arrays.asList(email2));
+		MongoCursor<Document> cursor = collection.find(Filters.and(senderFilter,toFilter)).iterator();
+		int count1 = 0, count2 = 0;
+		String logger = "";
+		while(cursor.hasNext()){
+			cursor.next();
+			count1++;
+			
+		}
+		logger += "("+count1+",";
 		cursor.close();
 		
-		senderFilter = new Document("headers.from",email2);
-		toFilter = Filters.in("headers.to", Arrays.asList(email1));
+		senderFilter = new Document("headers.From",email2);
+		toFilter = Filters.in("headers.To", Arrays.asList(email1));
 		cursor = collection.find(Filters.and(senderFilter,toFilter)).iterator();
 		
 		while(cursor.hasNext()){
-			count++;
+			count2++;
+			cursor.next();
 		}
-		
+		logger += count2+")";
 		cursor.close();
-		
-		return count;
+		System.out.print(logger);
+		return count1;
 	}
 	
 
